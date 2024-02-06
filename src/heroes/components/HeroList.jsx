ccb5199c8dca680c19704ce3b236548b255b5f8b
Miriam Valera <miriamvdaw@gmail.com>
2024-02-06 16:36:53 +0100

    Created new HeroCard component for add hero information

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,4 +1,5 @@
 import { getHeroesByPublisher } from "../helpers"
+import { HeroCard } from "./";
 
 export const HeroList = ({ publisher }) => {
 
@@ -8,9 +9,10 @@ export const HeroList = ({ publisher }) => {
         <ul>
             {
                 heroes.map( hero => (
-                    <li key={ hero.id }>
-                        { hero.superhero }
-                    </li>
+                    <HeroCard 
+                        key={ hero.id }
+                        { ...hero }
+                    /> 
                 ))
             }
         </ul>
