a2d44c3521469fa3f2f0b46bbee482d17ff34fdf
Miriam Valera <miriamvdaw@gmail.com>
2024-02-06 16:46:49 +0100

    refactor to show card info (various ways)

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,5 +1,14 @@
 import { Link } from "react-router-dom";
 
+const CharactersByHero = ({ alter_ego, characters }) => {
+    // if ( alter_ego === characters ) return (<></>);
+    // return <p>{ characters }</p>
+
+    return ( alter_ego === characters )
+    ? <></>
+    : <p>{ characters }</p>
+}
+
 export const HeroCard = ({
     id,
     superhero,
@@ -10,6 +19,7 @@ export const HeroCard = ({
 }) => {
 
     const heroImageUrl = `/assets/heroes/${ id }.jpg`;
+    // const charactersByHero = ( <p>{ characters }</p> );
 
     return (
         <div className="col">
@@ -22,9 +32,10 @@ export const HeroCard = ({
                         <div className="card-body">
                             <h5 className="card-title">{ superhero }</h5>
                             <p className="card-text">{ alter_ego }</p>
-                            {
-                                ( alter_ego !== characters ) && ( <p>{ characters }</p> )
-                            }
+                            {/* {
+                                ( alter_ego !== characters ) && charactersByHero
+                            } */}
+                            <CharactersByHero characters={ characters } alter_ego={ alter_ego } />
                             <p className="card-text">
                                 <small className="text-muted">{ first_appearance }</small>
                             </p>
