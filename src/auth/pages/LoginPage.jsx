01b3caf5a0eed48f45c7db26fdded4106eec830c
Miriam Valera <miriamvdaw@gmail.com>
2024-02-08 17:53:34 +0100

    save the last page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -9,9 +9,11 @@ export const LoginPage = () => {
 
     const onLogin = () => {
 
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
         login( 'Miriam Valera' );
 
-        navigate('/', {
+        navigate( lastPath, {
             replace: true
         });
     }
